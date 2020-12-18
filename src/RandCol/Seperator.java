b32f80d6c60bc3d3ee6f75c1e34db468ffa80935
Miao Yu <miaoyu@umich.edu>
2020-12-18 07:39:44 -0500

    add rcc2

--- src/RandCol/Seperator.java
+++ src/RandCol/Seperator.java
@@ -1,8 +1,6 @@
 package RandCol;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Seperator {
     private Graph gStar;
@@ -55,6 +53,7 @@ public class Seperator {
 //            System.out.println(i+":"+components.get(i));
             checkRCI1(components.get(i));
         }
+        //checkRCI2(gStar);
 
 
 
@@ -89,6 +88,44 @@ public class Seperator {
         }
     }
 
+
+    public void checkRCI2(Graph gStar){
+        double[][] graph = new double[n+1][n+1];
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < n; j++){
+                graph[i][j] = gStar.x[i][j];
+            }
+        }
+        for(int j = 1; j < n;j ++){
+            graph[0][j] = Math.max(0, gStar.x[0][j] - 1.0/Q);
+            graph[j][n] = Math.max(0, 1.0/Q - gStar.x[0][j]);
+        }
+        boolean[] set = minCut(graph, 0, n+1);
+        int count = 0;
+        for(boolean b : set) if(b) count++;
+        double tot = 0;
+        double tot2 = 0;
+        for(int i = 1; i < n; i++){
+            if(set[i]){
+                tot += gStar.x[0][i];
+            }else {
+                tot2 += gStar.x[0][i];
+            }
+        }
+        int r = (int)Math.ceil(1.0*(count)/Q);
+        if(tot < 2*r-1e-6){
+            Cut cut = new Cut(set, 2*r);
+            cutPool.add(cut);
+        }
+        r = (int) Math.ceil(1.0*(n-1-count)/Q);
+        if(tot2 < 2*r-1e-6){
+            boolean[] cset = new boolean[n];
+            for(int i = 1; i < n; i++) cset[i] = !set[i];
+            Cut cut = new Cut(cset, 2*r);
+            cutPool.add(cut);
+        }
+
+    }
     public List<Cut> generate(){
         roundedCap();
         return cutPool;
@@ -198,5 +235,105 @@ public class Seperator {
 
 
 
+    private static boolean bfs(double[][] rGraph, int s,
+                               int t, int[] parent) {
+
+        // Create a visited array and mark
+        // all vertices as not visited
+        boolean[] visited = new boolean[rGraph.length];
+
+        // Create a queue, enqueue source vertex
+        // and mark source vertex as visited
+        Queue<Integer> q = new LinkedList<Integer>();
+        q.add(s);
+        visited[s] = true;
+        parent[s] = -1;
+
+        // Standard BFS Loop
+        while (!q.isEmpty()) {
+            int v = q.poll();
+            for (int i = 0; i < rGraph.length; i++) {
+                if (rGraph[v][i] > 0 && !visited[i]) {
+                    q.offer(i);
+                    visited[i] = true;
+                    parent[i] = v;
+                }
+            }
+        }
+
+        // If we reached sink in BFS starting
+        // from source, then return true, else false
+        return (visited[t] == true);
+    }
+
+    // A DFS based function to find all reachable
+    // vertices from s. The function marks visited[i]
+    // as true if i is reachable from s. The initial
+    // values in visited[] must be false. We can also
+    // use BFS to find reachable vertices
+    private static void dfs(double[][] rGraph, int s,
+                            boolean[] visited) {
+        visited[s] = true;
+        for (int i = 0; i < rGraph.length; i++) {
+            if (rGraph[s][i] > 0 && !visited[i]) {
+                dfs(rGraph, i, visited);
+            }
+        }
+    }
+
+    // Prints the minimum s-t cut
+    private boolean[] minCut(double[][] graph, int s, int t) {
+        int u,v;
+
+        // Create a residual graph and fill the residual
+        // graph with given capacities in the original
+        // graph as residual capacities in residual graph
+        // rGraph[i][j] indicates residual capacity of edge i-j
+        double[][] rGraph = new double[graph.length][graph.length];
+        for (int i = 0; i < graph.length; i++) {
+            for (int j = 0; j < graph.length; j++) {
+                rGraph[i][j] = graph[i][j];
+            }
+        }
+
+        // This array is filled by BFS and to store path
+        int[] parent = new int[graph.length];
+
+        // Augment the flow while tere is path from source to sink
+        while (bfs(rGraph, s, t, parent)) {
+
+            // Find minimum residual capacity of the edhes
+            // along the path filled by BFS. Or we can say
+            // find the maximum flow through the path found.
+            double pathFlow = Double.POSITIVE_INFINITY;
+            for (v = t; v != s; v = parent[v]) {
+                u = parent[v];
+                pathFlow = Math.min(pathFlow, rGraph[u][v]);
+            }
+
+            // update residual capacities of the edges and
+            // reverse edges along the path
+            for (v = t; v != s; v = parent[v]) {
+                u = parent[v];
+                rGraph[u][v] = rGraph[u][v] - pathFlow;
+                rGraph[v][u] = rGraph[v][u] + pathFlow;
+            }
+        }
+
+        // Flow is maximum now, find vertices reachable from s
+        boolean[] isVisited = new boolean[graph.length];
+        dfs(rGraph, s, isVisited);
+
+        // Print all edges that are from a reachable vertex to
+        // non-reachable vertex in the original graph
+//        for (int i = 0; i < graph.length; i++) {
+//            for (int j = 0; j < graph.length; j++) {
+//                if (graph[i][j] > 0 && isVisited[i] && !isVisited[j]) {
+//                    System.out.println(i + " - " + j);
+//                }
+//            }
+//        }
+        return isVisited;
+    }
 
 }
